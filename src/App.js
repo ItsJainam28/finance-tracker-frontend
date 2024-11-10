8e2adc986318f3909bb5c7a405209659567e01cf
Jainam Patel <jainampatel68@gmail.com>
2024-11-09 20:08:14 -0500

    Made changes to the App.js file, added a new file Navbar.jsx, renamed Login.js to Login.jsx and Signup.js to Signup.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './styles/App.css';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       {/* <Signup></Signup> */}
       <Login></Login>
     </>
